8b8290661d80b9075eccee5e13ff0bf7bfda44db
axel castro <castroaxel.f@gmail.com>
2023-04-18 23:00:53 +0200

    ajout du mois en lettres du jour et de l'année dans l'horloge

--- src/components/Header/Clock.jsx
+++ src/components/Header/Clock.jsx
@@ -2,20 +2,25 @@ import { useState, useEffect } from 'react';
 
 const Clock = () => {
   const [time, setTime] = useState(new Date());
-
+  //creation du refresh par seconde
   useEffect(() => {
     setInterval(() => {
       setTime(new Date());
     }, 1000);
   }, []);
+  //condition pour que 1 sec soit ecrit en 01 par exemple
+  const zero = (i) => (i < 10 ? '0' + i : i);
 
-  const hours = time.getHours();
-  const minutes = time.getMinutes();
-  const seconds = time.getSeconds();
+  const hours = zero(time.getHours());
+  const minutes = zero(time.getMinutes());
+  const seconds = zero(time.getSeconds());
+  const date = zero(time.getDate());
+  const year = time.getFullYear();
+  const month = time.toLocaleString('fr-FR', { month: 'long' });
 
   return (
     <div>
-      {hours} : {minutes} : {seconds}
+      mardi {date} {month} {year} {hours} : {minutes} : {seconds}
     </div>
   );
 };
