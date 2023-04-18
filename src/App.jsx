eef02dc6a6286377dfe7a3e0f21f9b2cf1ce7d33
Coopykat <kathycamazon@gmail.com>
2023-04-18 12:16:22 +0200

    first version cards with button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
+import Cards from './components/Cards'
 
 function App() {
   return (
     <div className="App">
       Bienvenue sur le projet Planetarium !
+      <Cards />
     </div>
   );
 }
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,7 +1,51 @@
 import React from "react";
+import ExitButton from "./ExitButton";
+import './Cards.css';
+// import img from './src/assets/venus.png'
+
+
 
 const Cards = () => {
-  return <div>Cards</div>;
-};
+
+
+  return (
+  
+  
+<div className="Container-Cards">
+
+<ExitButton />
+
+<h1 className="Title">Venus</h1>
+
+<h2 className="Description">Planete tellurique</h2>
+
+{/* <img src={img} alt="venus"/> */}
+
+<p className="Presentation">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+Minus, maxime recusandae soluta, at necessitatibus alias officia
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit necessitatibus 
+officia qui voluptatum quos commodi 
+sint omnis ducimus error neque iste itaque reiciendis 
+velit id ut rerum, esse quas fuga.
+</p>
+
+    <div className="Moons">
+
+      <div className="MoonDesc">
+FBEZIQBFIQBGIBZQIBG
+      </div>
+
+      <div className="Facts">
+BRHBGEUIQGBEQIGBI
+      </div>
+
+    
+    </div>
+
+
+</div>
+
+
+)};
 
 export default Cards;
