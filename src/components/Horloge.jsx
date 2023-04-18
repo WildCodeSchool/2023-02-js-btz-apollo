a8bf950e3e8d4d016a8c691e8de4be0df34b4658
axel castro <castroaxel.f@gmail.com>
2023-04-18 10:13:08 +0200

    creation de l'architechture de l'appli + installation des dépendances json et router

---
+++ src/components/Horloge.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Horloge = () => {
+  return <div>Horloge</div>;
+};
+
+export default Horloge;
---
+++ src/pages/Scene.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Scene = () => {
+  return <div>Scene</div>;
+};
+
+export default Scene;
