833852e3dab8d60b91997ece2e4501f262ada274
Coopykat <kathycamazon@gmail.com>
2023-04-19 15:49:31 +0200

    css cards update

--- src/components/ExitButton.jsx
+++ src/components/ExitButton.jsx
@@ -1,16 +1,31 @@
 import React from 'react'
+import Scene from '../pages/Scene'
 import {CgClose} from "react-icons/cg"
 
 function ExitButton() {
+
+  const [isClicked, setIsClicked] = useState(true)
+
+
+
+
+
+  function handleClick(e) {
+    e.preventDefault();
+    console.log('Le lien a été cliqué.');
+    
+
+  }
+
+
   return (
 
-
-    <div>
+    <div onClick={handleClick}>
         <CgClose />
     </div>
   )
 
-  
+
 }
 
 export default ExitButton
