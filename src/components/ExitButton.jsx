64a638fac8cc0b1e633c8cecb7168c9ab72e5a7f
Coopykat <kathycamazon@gmail.com>
2023-04-19 16:00:20 +0200

    clean branch

--- src/components/ExitButton.jsx
+++ src/components/ExitButton.jsx
@@ -2,30 +2,12 @@ import React from 'react'
 import Scene from '../pages/Scene'
 import {CgClose} from "react-icons/cg"
 
-function ExitButton() {
 
-  const [isClicked, setIsClicked] = useState(true)
-
-
-
-
-
-  function handleClick(e) {
-    e.preventDefault();
-    console.log('Le lien a été cliqué.');
-    
-
-  }
-
-
-  return (
-
-    <div onClick={handleClick}>
-        <CgClose />
-    </div>
-  )
-
-
-}
-
-export default ExitButton
+ 
+ const ExitButton = () => {
+   return (
+     <div>ExitButton</div>
+   )
+ }
+ 
+ export default ExitButton
