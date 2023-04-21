c1d5dca6073e6df3cf21ed804f7e87c2eeaef489
nitramXmas <24914535+nitramXmas@users.noreply.github.com>
2023-04-21 09:40:34 +0200

    3D navbar integration

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,30 @@
+import { useEffect, useState } from 'react';
 import NavbarItems from './NavbarItems';
-import './Navbar.css'
+import './Navbar.css';
+import axios from 'axios';
 
 const Navbar = () => {
-  return <div className='navbar'>
-    <NavbarItems />
-  </div>;
+  const [objects, setObjects] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('https://apollo-api.martinnoel.fr/solar-system/solar-system')
+      .then((res) => setObjects(res.data.bodies));
+  }, []);
+
+  return (
+    <div className="navbar">
+      {objects &&
+        objects
+          .filter((obj) => obj.bodyType === 'Star')
+          .map((obj) => <NavbarItems key={obj.id} object={obj} />)}
+
+      {objects &&
+        objects
+          .filter((obj) => obj.bodyType === 'Planet')
+          .map((obj) => <NavbarItems key={obj.id} object={obj} />)}
+    </div>
+  );
 };
 
 export default Navbar;
--- src/components/navbar/NavbarItems.jsx
+++ src/components/navbar/NavbarItems.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
-import mercury from '../../assets/images/Mercury.png'
+import { useLoader } from '@react-three/fiber';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
 
-const NavbarItems = () => {
+import './NavbarItems.css';
+
+const NavbarItems = ({ object }) => {
+  const { model3d } = object;
+
+  const model = useLoader(GLTFLoader, model3d);
+
   return (
-    <div className='image-container'>
-      <img src={mercury} alt='mercury' />
+    <div className="object">
+      <p>{object.englishName}</p>
+      <Canvas camera={{ fov: 50 }}>
+        <OrbitControls />
+        <pointLight position={[5, 5, 5]} intensity={1} />
+        <primitive object={model.scene} scale={0.0025} />
+      </Canvas>
     </div>
-  )
-}
+  );
+};
 
-export default NavbarItems
+export default NavbarItems;
