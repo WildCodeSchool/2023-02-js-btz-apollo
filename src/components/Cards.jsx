a4c6e55ad737a3ca220c8da16e2022736ded6bf8
nitramXmas <24914535+nitramXmas@users.noreply.github.com>
2023-04-21 14:29:33 +0200

    resolve moon issues

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -4,30 +4,27 @@ import './Cards.css';
 
 const Cards = ({ planet }) => {
   return (
-    <div className='cards'>
-      <div className='container-title'>
-        <h1 className='title'>
+    <div className="cards">
+      <div className="container-title">
+        <h1 className="title">
           {planet.englishName}
           <ExitButton />
         </h1>
-        <h2 className='description'>{planet.bodyType}</h2>
+        <h2 className="description">{planet.bodyType}</h2>
       </div>
 
-      <div className='planet-image'>
-        <img
-          src={planet.image}
-          alt={planet.englishName}
-        />
+      <div className="planet-image">
+        <img src={planet.image} alt={planet.englishName} />
       </div>
-      <div className='bot-card'>
-        <p className='presentation'>{planet.englishDescription}</p>
+      <div className="bot-card">
+        <p className="presentation">{planet.englishDescription}</p>
         <h5>{planet.englishName} - Présentation</h5>
-        <div className='moon-container'>
-          <div className='moons'>
-            <div className='moon-title'>
+        <div className="moon-container">
+          <div className="moons">
+            <div className="moon-title">
               <h2> Moons</h2>
             </div>
-            <div className='moons-desc'>
+            <div className="moons-desc">
               <p>
                 blabla <br />
                 blablou <br />
