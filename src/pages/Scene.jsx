62b295b4196fd9b1b17e3e0822de5242d93c76ed
axel castro <castroaxel.f@gmail.com>
2023-04-21 10:40:02 +0200

    transitions des props pour affichage dynamique des cards et ajustement css en fonctions

--- src/pages/Scene.jsx
+++ src/pages/Scene.jsx
@@ -1,28 +1,31 @@
-import { useEffect, useState } from "react";
-import Cards from "../components/Cards";
-import axios from "axios";
-import "./Scene.css"
+import { useEffect, useState } from 'react';
+import Cards from '../components/Cards';
+import axios from 'axios';
+import './Scene.css';
 
 const Scene = () => {
   const [planet, setPlanet] = useState([]);
 
   useEffect(() => {
     axios
-      .get("https://apollo-api.martinnoel.fr/solar-system/solar-system")
-      .then((res) =>
-      setPlanet(res.data.bodies))
+      .get('https://apollo-api.martinnoel.fr/solar-system/solar-system')
+      .then((res) => setPlanet(res.data.bodies));
   }, []);
 
   // On a besoin du nom , image ,description ,les lunes associées , (facts)
   return (
-    <div className="scene">
+    <div className='scene'>
       {planet &&
-      planet
-      .filter((element)=> (element.bodyType==="Planet"))
-      .map((planet) => (<Cards key={planet.id} planet = {planet}/>
-      ))}
-  </div>
-  )
+        planet
+          .filter((element) => element.bodyType === 'Planet')
+          .map((planet) => (
+            <Cards
+              key={planet.id}
+              planet={planet}
+            />
+          ))}
+    </div>
+  );
 };
 
 export default Scene;
