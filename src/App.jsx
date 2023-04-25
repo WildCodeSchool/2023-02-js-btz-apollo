92e97d358989b9b9a71e374737d6dbc3c56f1b13
nitramXmas <24914535+nitramXmas@users.noreply.github.com>
2023-04-25 14:06:06 +0200

    initiate 3d scene on full screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import Header from './components/Header/Header';
 import Scene from './pages/Scene';
-import Navbar from './components/Navbar/Navbar';
 import './App.css';
 
 function App() {
     return (
         <div className='app'>
-            <Scene />
+                <Scene />
         </div>
     );
 }
--- src/pages/Scene.jsx
+++ src/pages/Scene.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
-import CardList from '../components/CardList/CardList'
+import { Canvas } from '@react-three/fiber'
+import { Stars, OrbitControls } from '@react-three/drei'
 import Header from '../components/Header/Header'
 import Navbar from '../components/Navbar/Navbar'
+import './Scene.css'
 
 const Scene = () => {
   return (
-    <>
+    <div className='scene'>
         <Header />
-        <CardList />
+            <div className="canvas">
+                <Canvas>
+                    <Stars radius={100} depth={50} count={15000} factor={4} saturation={0} fade speed={1} />
+                    <OrbitControls makeDefault />
+                </Canvas>
+            </div>
         <Navbar />
-    </>
+    </div>
   )
 }
 
