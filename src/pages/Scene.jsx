786bd48a8240854547e01a424667627e2c7f4621
axel castro <castroaxel.f@gmail.com>
2023-04-27 10:57:11 +0200

    loader done

--- src/pages/Scene.jsx
+++ src/pages/Scene.jsx
@@ -9,46 +9,55 @@ import Planet from '../components/Objects/Planet'
 import './Scene.css'
 
 const Scene = () => {
-
-
     const [objects, setObjects] = useState([]);
-
+    const [isLoading, setIsLoading] = useState(true);
     useEffect(() => {
-    axios
+      axios
         .get('https://apollo-api.martinnoel.fr/solar-system/solar-system')
-        .then((res) => setObjects(res.data.bodies));
+        .then((res) => {
+          setObjects(res.data.bodies);
+          setIsLoading(false);
+        });
     }, []);
-
-
-  return (
-    <div className='scene'>
-        <Header />
-            <div className="canvas">
-                <Canvas
-                    camera={{
-                        position: [-4000,2000,4000],
-                        fov: 45,
-                        near: 0.1,
-                        far: 999999999999999
-                    }}
-                >
-                    <Stars radius={10000} depth={50} count={30000} factor={7} saturation={3} fade speed={0} />
-                    <OrbitControls makeDefault />
-                    <pointLight />
-                    {objects &&
-                        objects
-                        .filter((object) => object.bodyType === 'Star')
-                        .map((star) => <Sun key = {star.id} sun = {star} />)}
-
-                    {objects &&
-                        objects
-                        .filter((object) => object.bodyType === 'Planet')
-                        .map((planet) => <Planet key = {planet.id} planet = {planet} />)}
-                </Canvas>
-            </div>
+    return (
+        <div className='scene'>
+          <Header />
+        <div className="canvas">
+          {isLoading ? (
+            <div className='loading'>Chargement...</div>
+          ) : (
+            <Canvas
+              camera={{
+                position: [0, 20, 40],
+                fov: 45,
+                near: 0.1,
+                far: 999999999999
+              }}
+            >
+              <Stars
+                radius={500}
+                depth={50}
+                count={5000}
+                factor={20}
+                saturation={1}
+                fade
+                speed={0}
+              />
+              <OrbitControls makeDefault />
+              <pointLight intensity={0.5} />
+              {objects &&
+                objects
+                  .filter((object) => object.bodyType === 'Star')
+                  .map((star) => <Sun key={star.id} sun={star} />)}
+              {objects &&
+                objects
+                  .filter((object) => object.bodyType === 'Planet')
+                  .map((planet) => <Planet key={planet.id} planet={planet} />)}
+            </Canvas>
+          )}
+        </div>
         <Navbar />
-    </div>
-  )
-}
-
-export default Scene
+      </div>
+    );
+  };
+  export default Scene;
